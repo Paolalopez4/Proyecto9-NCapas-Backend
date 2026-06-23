c48823dd182f42789b1c03917ef66c7e244b61a8
Paolalopez4 <00167523@uca.edu.sv>
2026-06-23 00:34:44 -0600

    feat: add serviceRequest dto

--- Auto_Repair_Shop/src/main/java/com/grupo9/auto_repair_shop/dto/request/service/ServiceRequest.java
+++ Auto_Repair_Shop/src/main/java/com/grupo9/auto_repair_shop/dto/request/service/ServiceRequest.java
@@ -1,4 +1,24 @@
 package com.grupo9.auto_repair_shop.dto.request.service;
 
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.*;
+
+import java.math.BigDecimal;
+
 public class ServiceRequest {
+    @NotBlank(message = "Name is required")
+    @Size(max = 150, message = "Name must be at most 150 characters")
+    private String name;
+
+    @Size(max = 1000, message = "Description must be at most 1000 characters")
+    private String description;
+
+    @NotNull(message = "Base price is required")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Base price must be greater than 0")
+    @Digits(integer = 8, fraction = 2, message = "Base price must be a valid monetary amount")
+    private BigDecimal basePrice;
+
+    @NotNull(message = "Estimated minutes is required")
+    @Positive(message = "Estimated minutes must be a positive integer")
+    private Integer estimatedMinutes;
 }
