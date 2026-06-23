1a63c8a5e24ff2677631e4dc8c99237a44f617f3
Paolalopez4 <00167523@uca.edu.sv>
2026-06-23 02:54:57 -0600

    feat: add supplier mapper

--- Auto_Repair_Shop/src/main/java/com/grupo9/auto_repair_shop/mapper/supplier/SupplierMapper.java
+++ Auto_Repair_Shop/src/main/java/com/grupo9/auto_repair_shop/mapper/supplier/SupplierMapper.java
@@ -1,4 +1,14 @@
 package com.grupo9.auto_repair_shop.mapper.supplier;
 
-public class SupplierMapper {
+import com.grupo9.auto_repair_shop.dto.request.supplier.SupplierRequest;
+import com.grupo9.auto_repair_shop.dto.response.supplier.SupplierResponse;
+import com.grupo9.auto_repair_shop.entity.supplier.Supplier;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public interface SupplierMapper {
+    Supplier toEntity(SupplierRequest supplierRequest);
+    SupplierResponse toResponse(Supplier supplier);
+    void updateEntity(SupplierRequest supplierRequest, @MappingTarget Supplier supplier);
 }
