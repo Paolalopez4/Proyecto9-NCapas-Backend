606911166ad9e8aaa15f6cdc4b71246fb9360e45
Paolalopez4 <00167523@uca.edu.sv>
2026-06-23 02:10:40 -0600

    feat: add servicecontroller endpoints

--- Auto_Repair_Shop/src/main/java/com/grupo9/auto_repair_shop/dto/response/service/ServiceResponse.java
+++ Auto_Repair_Shop/src/main/java/com/grupo9/auto_repair_shop/dto/response/service/ServiceResponse.java
@@ -1,8 +1,15 @@
 package com.grupo9.auto_repair_shop.dto.response.service;
 
+import lombok.*;
+
 import java.math.BigDecimal;
 import java.util.UUID;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ServiceResponse {
     private UUID id;
     private String name;
