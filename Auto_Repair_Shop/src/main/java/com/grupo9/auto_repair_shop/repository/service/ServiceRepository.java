7e117803a715e4a2382208acb9588e607faf975c
Paolalopez4 <00167523@uca.edu.sv>
2026-06-23 00:48:34 -0600

    feat: add servicerepository with personalized methods

--- Auto_Repair_Shop/src/main/java/com/grupo9/auto_repair_shop/repository/service/ServiceRepository.java
+++ Auto_Repair_Shop/src/main/java/com/grupo9/auto_repair_shop/repository/service/ServiceRepository.java
@@ -1,4 +1,25 @@
 package com.grupo9.auto_repair_shop.repository.service;
 
-public interface ServiceRepository {
+import com.grupo9.auto_repair_shop.entity.service.Service;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface ServiceRepository extends JpaRepository<Service, UUID> {
+    //listar con filtros
+    Page<Service> findByNameContainingIgnoreCaseAndActive(String name, Boolean active, Pageable pageable);
+    Page<Service> findByNameContainingIgnoreCase(String name, Pageable pageable);
+    Page<Service> findByActive(Boolean active, Pageable pageable);
+
+    //validaciones de nombre
+    boolean existsByNameIgnoreCase(String name);
+    boolean existsByNameIgnoreCaseAndIdNot(String name, UUID id);
+
+    //busqueda por nombre exacto
+    Optional<Service> findByNameIgnoreCase(String name);
 }
