4cdc0d7eb8fc3a71b11c35ae64fbc513b1d05f28
Paolalopez4 <00167523@uca.edu.sv>
2026-06-23 22:44:52 -0600

    feat: add repairhistory service methods

--- Auto_Repair_Shop/src/main/java/com/grupo9/auto_repair_shop/service/repairhistory/RepairHistoryService.java
+++ Auto_Repair_Shop/src/main/java/com/grupo9/auto_repair_shop/service/repairhistory/RepairHistoryService.java
@@ -1,4 +1,15 @@
 package com.grupo9.auto_repair_shop.service.repairhistory;
 
+import com.grupo9.auto_repair_shop.dto.response.repairhistory.RepairHistoryResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
 public interface RepairHistoryService {
+    Page<RepairHistoryResponse> findAll(LocalDate from, LocalDate to, Pageable pageable);
+    RepairHistoryResponse findById(UUID id);
+    List<RepairHistoryResponse> findByVehicleId(UUID vehicleId);
 }
