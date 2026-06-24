5660ffd3b7e06eb8d3c64587f96fb576ef1f9a6a
rebecaeta <00225023@uca.edu.sv>
2026-06-23 19:26:47 -0600

    feat: add clientResponse dto

--- Auto_Repair_Shop/src/main/java/com/grupo9/auto_repair_shop/dto/response/client/ClientResponse.java
+++ Auto_Repair_Shop/src/main/java/com/grupo9/auto_repair_shop/dto/response/client/ClientResponse.java
@@ -1,4 +1,20 @@
 package com.grupo9.auto_repair_shop.dto.response.client;
+import lombok.Builder;
+import lombok.Data;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
 public class ClientResponse {
+
+    private UUID id;
+    private String phone;
+    private String address;
+    private LocalDateTime createdAt;
+
+    private UUID userId;
+    private String userName;
+    private String userEmail;
 }
