998297de23d117cd7c940ab6bb6677650b4bfe5a
Paolalopez4 <00167523@uca.edu.sv>
2026-06-23 22:41:12 -0600

    feat: add to repairhistory dto lombok annotations

--- Auto_Repair_Shop/src/main/java/com/grupo9/auto_repair_shop/dto/response/repairhistory/RepairHistoryResponse.java
+++ Auto_Repair_Shop/src/main/java/com/grupo9/auto_repair_shop/dto/response/repairhistory/RepairHistoryResponse.java
@@ -3,10 +3,16 @@ package com.grupo9.auto_repair_shop.dto.response.repairhistory;
 import com.grupo9.auto_repair_shop.entity.vehicle.Vehicle;
 import com.grupo9.auto_repair_shop.entity.workorder.WorkOrder;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class RepairHistoryResponse {
     private UUID id;
     private String summary;
