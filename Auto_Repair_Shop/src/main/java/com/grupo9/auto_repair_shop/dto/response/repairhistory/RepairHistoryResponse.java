8dcbdf95d139dcb67847d000669b52d8eee50e8f
Paolalopez4 <00167523@uca.edu.sv>
2026-06-23 22:40:42 -0600

    feat: add repairhistory dto

--- Auto_Repair_Shop/src/main/java/com/grupo9/auto_repair_shop/dto/response/repairhistory/RepairHistoryResponse.java
+++ Auto_Repair_Shop/src/main/java/com/grupo9/auto_repair_shop/dto/response/repairhistory/RepairHistoryResponse.java
@@ -1,4 +1,20 @@
 package com.grupo9.auto_repair_shop.dto.response.repairhistory;
 
+import com.grupo9.auto_repair_shop.entity.vehicle.Vehicle;
+import com.grupo9.auto_repair_shop.entity.workorder.WorkOrder;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
 public class RepairHistoryResponse {
+    private UUID id;
+    private String summary;
+    private LocalDate repairDate;
+    private Integer mileageAtRepair;
+    private UUID vehicleId;
+    private String vehiclePlate;
+    private String vehicleBrand;
+    private String vehicleModel;
+    private UUID workOrderId;
 }
