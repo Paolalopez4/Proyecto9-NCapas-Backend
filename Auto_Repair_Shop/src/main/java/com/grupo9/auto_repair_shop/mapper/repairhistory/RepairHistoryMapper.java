933d5f02d2ef6b6de8a31656e17644487b967f78
Paolalopez4 <00167523@uca.edu.sv>
2026-06-23 22:41:58 -0600

    feat: add repairhistory mapper

--- Auto_Repair_Shop/src/main/java/com/grupo9/auto_repair_shop/mapper/repairhistory/RepairHistoryMapper.java
+++ Auto_Repair_Shop/src/main/java/com/grupo9/auto_repair_shop/mapper/repairhistory/RepairHistoryMapper.java
@@ -1,4 +1,17 @@
 package com.grupo9.auto_repair_shop.mapper.repairhistory;
 
-public class RepairHistoryMapper {
+import com.grupo9.auto_repair_shop.dto.response.repairhistory.RepairHistoryResponse;
+import com.grupo9.auto_repair_shop.entity.repairhistory.RepairHistory;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface RepairHistoryMapper {
+
+    @Mapping(source = "vehicle.id", target = "vehicleId")
+    @Mapping(source = "vehicle.plate", target = "vehiclePlate")
+    @Mapping(source = "vehicle.brand", target = "vehicleBrand")
+    @Mapping(source = "vehicle.model", target = "vehicleModel")
+    @Mapping(source = "workOrder.id", target = "workOrderId")
+    RepairHistoryResponse toResponse(RepairHistory repairHistory);
 }
