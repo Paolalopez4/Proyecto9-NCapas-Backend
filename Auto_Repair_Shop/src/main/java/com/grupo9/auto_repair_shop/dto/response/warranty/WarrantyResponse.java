089f20e6da653b1042b1a5ff5320eb804bf96e9a
JJMELGAR04 <00073923@uca.edu.sv>
2026-06-24 14:57:16 -0600

    feat: add warranty DTOs

--- Auto_Repair_Shop/src/main/java/com/grupo9/auto_repair_shop/dto/response/warranty/WarrantyResponse.java
+++ Auto_Repair_Shop/src/main/java/com/grupo9/auto_repair_shop/dto/response/warranty/WarrantyResponse.java
@@ -1,4 +1,24 @@
 package com.grupo9.auto_repair_shop.dto.response.warranty;
 
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class WarrantyResponse {
+
+    private UUID id;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String coverage;
+    private Boolean active;
+    private UUID workOrderId;
+    private UUID vehicleId;
+    private String vehiclePlate;
+    private Boolean expired;
 }
