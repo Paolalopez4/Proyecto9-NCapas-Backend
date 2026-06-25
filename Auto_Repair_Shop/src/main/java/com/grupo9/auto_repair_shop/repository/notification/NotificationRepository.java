db28c64f1a0d20d30de965a0a7d5b77095852137
JJMELGAR04 <00073923@uca.edu.sv>
2026-06-24 22:44:35 -0600

    feat: add notification repository

--- Auto_Repair_Shop/src/main/java/com/grupo9/auto_repair_shop/repository/notification/NotificationRepository.java
+++ Auto_Repair_Shop/src/main/java/com/grupo9/auto_repair_shop/repository/notification/NotificationRepository.java
@@ -1,4 +1,15 @@
 package com.grupo9.auto_repair_shop.repository.notification;
 
-public interface NotificationRepository {
+import com.grupo9.auto_repair_shop.entity.notification.Notification;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface NotificationRepository extends JpaRepository<Notification, UUID> {
+
+    Page<Notification> findByUserId(UUID userId, Pageable pageable);
+
+    Page<Notification> findByUserEmail(String email, Pageable pageable);
 }
