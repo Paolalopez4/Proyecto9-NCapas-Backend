08205d7bafabaf61482451e168d3fe9e7e0cc9c5
rebecaeta <00225023@uca.edu.sv>
2026-06-25 00:22:28 -0600

    feat: add vehiclemapper using mapstruct for entity updates and responses

--- Auto_Repair_Shop/src/main/java/com/grupo9/auto_repair_shop/mapper/vehicle/VehicleMapper.java
+++ Auto_Repair_Shop/src/main/java/com/grupo9/auto_repair_shop/mapper/vehicle/VehicleMapper.java
@@ -1,4 +1,26 @@
 package com.grupo9.auto_repair_shop.mapper.vehicle;
 
-public class VehicleMapper {
+import com.grupo9.auto_repair_shop.dto.request.vehicle.UpdateVehicleRequest;
+import com.grupo9.auto_repair_shop.dto.request.vehicle.VehicleRequest;
+import com.grupo9.auto_repair_shop.dto.response.vehicle.VehicleResponse;
+import com.grupo9.auto_repair_shop.entity.vehicle.Vehicle;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public interface VehicleMapper {
+
+        @Mapping(target = "clientId", source = "client.id")
+        @Mapping(target = "clientName", source = "client.user.name")
+        VehicleResponse toResponse(Vehicle vehicle);
+
+        @Mapping(target = "plate", source = "request.plate")
+        @Mapping(target = "brand", source = "request.brand")
+        @Mapping(target = "model", source = "request.model")
+        @Mapping(target = "year", source = "request.year")
+        @Mapping(target = "color", source = "request.color")
+        @Mapping(target = "mileage", source = "request.mileage")
+        void updateEntity(UpdateVehicleRequest request, @MappingTarget Vehicle vehicle);
+
 }
