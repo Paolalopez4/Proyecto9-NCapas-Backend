2d304335bcbc462a6e781df9342742ea857e1428
Paolalopez4 <00167523@uca.edu.sv>
2026-06-26 19:30:34 -0600

    feat: add appointment dtos

--- Auto_Repair_Shop/src/main/java/com/grupo9/auto_repair_shop/dto/response/appointment/AppointmentResponse.java
+++ Auto_Repair_Shop/src/main/java/com/grupo9/auto_repair_shop/dto/response/appointment/AppointmentResponse.java
@@ -1,4 +1,31 @@
 package com.grupo9.auto_repair_shop.dto.response.appointment;
 
+import com.grupo9.auto_repair_shop.enums.AppointmentStatus;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class AppointmentResponse {
+    private UUID id;
+    private LocalDateTime scheduledAt;
+    private AppointmentStatus status;
+    private String notes;
+    private UUID clientId;
+    private String clientName;
+    private UUID vehicleId;
+    private String vehiclePlate;
+    private String vehicleBrand;
+    private String vehicleModel;
+    private UUID branchId;
+    private String branchName;
+    private UUID mechanicId;
+    private String mechanicName;
+    private UUID workOrderId;
 }
