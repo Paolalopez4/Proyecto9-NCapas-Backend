05da4f0d4603190c4742c43b4547039e4a62f472
rebecaeta <00225023@uca.edu.sv>
2026-06-27 03:13:33 -0600

    feat: update maintenance reminder repository

--- Auto_Repair_Shop/src/main/java/com/grupo9/auto_repair_shop/repository/reminder/MaintenanceReminderRepository.java
+++ Auto_Repair_Shop/src/main/java/com/grupo9/auto_repair_shop/repository/reminder/MaintenanceReminderRepository.java
@@ -1,4 +1,20 @@
 package com.grupo9.auto_repair_shop.repository.reminder;
 
-public interface MaintenanceReminderRepository {
+import com.grupo9.auto_repair_shop.entity.reminder.MaintenanceReminder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface MaintenanceReminderRepository extends JpaRepository<MaintenanceReminder, UUID> {
+
+    Page<MaintenanceReminder> findByVehicleId(UUID vehicleId, Pageable pageable);
+
+    Page<MaintenanceReminder> findByClientId(UUID clientId, Pageable pageable);
+
+    Page<MaintenanceReminder> findBySent(Boolean sent, Pageable pageable);
+
+    Page<MaintenanceReminder> findByAcknowledged(Boolean acknowledged, Pageable pageable);
+
 }
