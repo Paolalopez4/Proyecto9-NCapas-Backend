aba0da646170f1404a4288886cfb2d4ff3d667dc
rebecaeta <00225023@uca.edu.sv>
2026-06-27 03:12:58 -0600

    feat: update maintenance reminder mapper

--- Auto_Repair_Shop/src/main/java/com/grupo9/auto_repair_shop/mapper/reminder/MaintenanceReminderMapper.java
+++ Auto_Repair_Shop/src/main/java/com/grupo9/auto_repair_shop/mapper/reminder/MaintenanceReminderMapper.java
@@ -1,4 +1,26 @@
 package com.grupo9.auto_repair_shop.mapper.reminder;
 
-public class MaintenanceReminderMapper {
+import com.grupo9.auto_repair_shop.dto.request.reminder.MaintenanceReminderUpdateRequest;
+import com.grupo9.auto_repair_shop.dto.response.reminder.MaintenanceReminderResponse;
+import com.grupo9.auto_repair_shop.entity.reminder.MaintenanceReminder;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
+public interface MaintenanceReminderMapper {
+
+        @Mapping(target = "vehicleId",   source = "vehicle.id")
+        @Mapping(target = "vehiclePlate", source = "vehicle.plate")
+        @Mapping(target = "vehicleBrand", source = "vehicle.brand")
+        @Mapping(target = "clientId",    source = "client.id")
+        @Mapping(target = "clientName",  source = "client.user.name")
+        MaintenanceReminderResponse toResponse(MaintenanceReminder reminder);
+
+        @Mapping(target = "reminderType", source = "request.reminderType")
+        @Mapping(target = "dueDate",      source = "request.dueDate")
+        @Mapping(target = "dueMileage",   source = "request.dueMileage")
+        @Mapping(target = "notes",        source = "request.notes")
+        void updateEntity(MaintenanceReminderUpdateRequest request, @MappingTarget MaintenanceReminder reminder);
+
 }
